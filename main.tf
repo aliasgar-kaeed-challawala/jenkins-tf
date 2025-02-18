37bf597f2976bad89679b0b469efc32a1349d31a
Challawala, Aliasgar Kaeed <achallawala@ehe.health>
2025-02-18 18:06:26 +0530

    removed ec2 block

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-data "aws_ami" "amazon_linux_2023" {
-  most_recent = true
-  owners      = ["amazon"]
-  name_regex  = "al2023-ami-2023"
-}
-
 resource "aws_s3_bucket" "akc-jenkins-tf-bucket" {
   bucket = "akc-jenkins-bucket-tf"
     tags = {
@@ -12,11 +6,3 @@ resource "aws_s3_bucket" "akc-jenkins-tf-bucket" {
     }
 }
 
-
-resource "aws_instance" "akc-jenkins-tf-instance" {
-  ami             = data.aws_ami.amazon_linux_2023.id
-  instance_type = "t3.micro"
-  tags = {
-    Name = "akc-jenkins-tf-instance"
-  }
-}
