f438dcfc07e60b09cc297bacb5ba7de1921a062a
Challawala, Aliasgar Kaeed <achallawala@ehe.health>
2025-02-18 16:00:45 +0530

    renamed the s3 bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "akc-jenkins-tf-bucket" {
-  bucket = "akc-jenkins-tf-bucket"
+  bucket = "akc-jenkins-bucket-tf"
     tags = {
         createdBy        = "achallawala@presidio.com"
         Purpose          = "Jenkins Training"
