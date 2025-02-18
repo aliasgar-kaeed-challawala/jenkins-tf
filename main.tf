9b73a25dbbdefff185f5d65aa70b3c0144b45f8a
Challawala, Aliasgar Kaeed <achallawala@ehe.health>
2025-02-18 16:30:43 +0530

    Added script to include ec2 instance

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  name_regex  = "al2023-ami-2023"
+}
+
 resource "aws_s3_bucket" "akc-jenkins-tf-bucket" {
   bucket = "akc-jenkins-bucket-tf"
     tags = {
         createdBy        = "achallawala@presidio.com"
         Purpose          = "Jenkins Training"
     }
+}
+
+resource "aws_instance" "akc-jenkins-tf-instance" {
+  ami             = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "akc-jenkins-tf-instance"
+  }
 }
